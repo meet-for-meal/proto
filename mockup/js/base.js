c7edb0dbc11acb82d27aa27426b45c4efa4e52d4
Hervé Tran <contact@hervetran.fr>
2013-02-28 15:43:16 +0100

    Add Notification center, jQuery and modifiy design

---
+++ mockup/js/base.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('html').click(function() {
+        //Hide the menus if visible
+        $('#notifications').slideUp(200);
+    });
+
+    $('.toggle-notif').click(function(e){
+        e.stopPropagation();
+        $('#notifications').slideToggle(200);
+        $(this).children('.notif').remove();
+    });
+
+});
