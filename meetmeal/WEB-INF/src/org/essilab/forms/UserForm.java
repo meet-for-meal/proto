1030c9a698fec8ac3349fa23e50b4401395080fa
Kirua88 <souks.ricky@gmail.com>
2013-05-22 21:57:32 +0200

    mypage

---
+++ meetmeal/WEB-INF/src/org/essilab/forms/UserForm.java
@@ -0,0 +1,65 @@
+package org.essilab.forms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.essilab.module.interest.model.Interest;
+
+public class UserForm {
+	private static final String INTERESTS   = "interests";
+    private String              result;
+    private Map<String, String> errors      = new HashMap<String, String>();
+
+     
+    public String getResult() {
+        return result;
+    }
+     
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+    
+    
+    public List<Interest> SearchInterest( HttpServletRequest request ) {
+    	String interests = getFieldValue( request, INTERESTS );
+    	if(interests != null){
+    		List<Interest> list = new ArrayList<Interest>();
+    		String str[]=interests.split(",");
+    		for(String s : str){
+    			Interest in = new Interest(s);
+    			list.add(in);
+    		}
+    		return list;
+    	}
+    	else{
+    		System.out.println("UserForm.java");
+    	}
+    	return null;
+    }
+	
+    
+    
+    /*
+     * Add message to map error
+     */
+    private void setError( String champ, String message ) {
+        errors.put( champ, message );
+    }
+    
+    
+    /*
+     * Return field value or null
+     */
+    private static String getFieldValue( HttpServletRequest request, String fieldName ) {
+        String valeur = request.getParameter( fieldName );
+        if ( valeur == null || valeur.trim().length() == 0 ) {
+            return null;
+        } else {
+            return valeur.trim();
+        }
+    }
+}
