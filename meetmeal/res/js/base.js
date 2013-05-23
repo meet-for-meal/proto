3f3c72c12b0e0853ebcbffdeb3e96fff7c286140
Hervé Tran <contact@hervetran.fr>
2013-05-23 17:45:00 +0200

    Added notification alert system with cool animations

--- meetmeal/res/js/base.js
+++ meetmeal/res/js/base.js
@@ -2,16 +2,55 @@ mfm = {
     host: 'meetformeal',
     config: {
         debug: false
+    },
+    actions: {
+        popAlert : function(req){
+
+            var type = '';
+            var text = '';
+
+            switch (req) {
+                case 'accept-request':
+                    type = 'succes';
+                    text = 'Félicitations ! La demande d\'amitié a été confirmée.';
+                    break;
+                case 'ignore-request':
+                    type = 'erreur';
+                    text = 'La demande d\'amitié a été ignorée.';
+                    break;
+                case 'friend-request':
+                    type = 'succes';
+                    text = 'Demande d\'amitié envoyée';
+                    break;
+                default:
+            }
+            var alert = '<div class="alert '+type+'"><a class="close" href="#">x</a>'+text+'</div>';
+
+            if($('#alerts').length !== 0){
+                $('#alerts').html('');
+                $('#alerts').prepend(alert);
+                $('.alert').slideDown(300);
+            } else {
+                $('body').prepend('<div id="alerts">'+alert+'</div>');
+                $('.alert').slideDown(300);
+            }
+
+            setTimeout(function(){
+                jQuery('.alert').slideUp(300);
+            },5000 );
+
+        }
     }
 };
 
 $(document).ready(function(){
 
     // Home page
-
-    $('.close').click(function(e){
-        e.preventDefault();
-        $(this).parent('.alert').fadeOut(300);
+    $('body')
+        .off('click', '.close')
+        .on('click', '.close', function(e){
+            e.preventDefault();
+            $(this).parent('.alert').fadeOut(300);
     });
 
     $('.message.erreur').each(function(e){
@@ -22,7 +61,6 @@ $(document).ready(function(){
        });
 
     });
-
 
     // General
 
@@ -35,6 +73,37 @@ $(document).ready(function(){
         e.stopPropagation();
         $('#notifications').slideToggle(200);
         $(this).children('.notif').remove();
+    });
+
+    $('#notifications').click(function(e){
+        e.stopPropagation();
+
+    });
+
+    $('.mfm-action').click(function(e){
+        e.preventDefault();
+        var self = $(this);
+        var actionType = $(this).attr('data-action');
+        if(typeof actionType !== 'undefined' && actionType.length !== 0){
+            switch (actionType) {
+                case 'accept-request':
+                    if(self.parent().hasClass('notification-action')){
+                        self.closest('li').slideUp(200);
+                    }
+                    mfm.actions.popAlert(actionType);
+                    break;
+                case 'ignore-request':
+                    if(self.parent().hasClass('notification-action')){
+                        self.closest('li').slideUp(200);
+                    }
+                    mfm.actions.popAlert(actionType);
+                    break;
+                case 'friend-request':
+                    mfm.actions.popAlert(actionType);
+                    break;
+                default:
+            }
+        }
     });
 
     $('#toggle-map').click(function(){
